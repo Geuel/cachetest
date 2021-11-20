1b323022839786c46a6593cc0c2e06d06b940cb9
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-11-20 00:03:23 -0300

    Arquivos iniciais do projeto

---
+++ src/main/java/com/softhousesolutions/cachetest/CachetestApplication.java
@@ -0,0 +1,15 @@
+package com.softhousesolutions.cachetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class CachetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachetestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softhousesolutions/cachetest/CachetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softhousesolutions.cachetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachetestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
