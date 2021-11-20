6aba5b3069010f515cdce8c5e4482b041d5bd8ab
Geuel Chaves Paiva <geuelcp@gmail.com>
2021-11-20 12:03:02 -0300

    Configuracao do REDIS

--- src/main/java/com/softhousesolutions/cachetest/entity/Product.java
+++ src/main/java/com/softhousesolutions/cachetest/entity/Product.java
@@ -3,11 +3,12 @@ package com.softhousesolutions.cachetest.entity;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 
 @Builder
 @Data
-public class Product {
+public class Product implements Serializable {
 
     private String sku;
     private BigDecimal price;
